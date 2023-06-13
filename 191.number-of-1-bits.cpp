c86982f9b030d7720c8d46b3eaf1f0021a181497
Scurrra <iscurrra@gmail.com>
2023-06-13 14:04:45 +0300

    191. Number of Bits

---
+++ 191.number-of-1-bits.cpp
@@ -0,0 +1,25 @@
+#include <cstdint>
+
+/*
+ * @lc app=leetcode id=191 lang=cpp
+ *
+ * [191] Number of 1 Bits
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        uint8_t ones = 0;
+        while (n > 0)
+        {
+            ones += n % 2;
+            n /= 2;
+        }
+        
+        return ones;
+    }
+};
+// @lc code=end
