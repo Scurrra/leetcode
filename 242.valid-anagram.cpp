7db093b7d79d6a104208e8c7d1b8f9985bd6d124
Scurrra <iscurrra@gmail.com>
2023-06-13 10:15:58 +0300

    242. Valid Anagram

--- 242.valid-anagram.cpp
+++ 242.valid-anagram.cpp
@@ -5,15 +5,17 @@
  * @lc app=leetcode id=242 lang=cpp
  *
  * [242] Valid Anagram
- * 
+ *
  * Space complexity: O(52) = O(1)
  * Time complexity: O(n + 26) = O(n)
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool isAnagram(std::string s, std::string t) {
+    bool isAnagram(std::string s, std::string t)
+    {
         if (s.length() != t.length())
         {
             return false;
@@ -22,10 +24,10 @@ public:
         std::array<int, 26> s_letters = {}, t_letters = {};
         for (size_t i = 0; i < s.length(); i++)
         {
-            s_letters[s[i]-'a']++;
-            t_letters[t[i]-'a']++;
+            s_letters[s[i] - 'a']++;
+            t_letters[t[i] - 'a']++;
         }
-        
+
         for (size_t i = 0; i < 26; i++)
         {
             if (s_letters[i] != t_letters[i])
@@ -33,9 +35,8 @@ public:
                 return false;
             }
         }
-        
+
         return true;
     }
 };
 // @lc code=end
-
