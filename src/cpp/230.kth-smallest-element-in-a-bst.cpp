e46c4ab57f5117c87513fcef9055b681850ccc27
Scurrra <iscurrra@gmail.com>
2023-09-08 09:28:20 +0300

    230. Kth Smallest Element in a BST

---
+++ src/cpp/230.kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,58 @@
+#include <stack>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/*
+ * @lc app=leetcode id=230 lang=cpp
+ *
+ * [230] Kth Smallest Element in a BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int kthSmallest(TreeNode *root, int k)
+    {
+        std::stack<TreeNode *> stack;
+        auto current = root;
+
+        while (!stack.empty() || current != nullptr)
+        {
+            while (current != nullptr)
+            {
+                stack.push(current);
+                current = current->left;
+            }
+            current = stack.top();
+            stack.pop();
+            if (--k == 0)
+            {
+                return current->val;
+            }
+            current = current->right;
+        }
+
+        return -1;
+    }
+};
+// @lc code=end
