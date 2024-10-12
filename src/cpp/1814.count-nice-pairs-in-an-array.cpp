198c80ac1bf122874c1e9ebeedc421713a5ebf4c
Scurrra <iscurrra@gmail.com>
2024-10-12 18:11:30 +0300

    1814. Count Nice Pairs in an Array

---
+++ src/cpp/1814.count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <unordered_map>
+#include <cstdint>
+
+/*
+ * @lc app=leetcode id=1814 lang=cpp
+ *
+ * [1814] Count Nice Pairs in an Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    uint64_t countNicePairs(std::vector<int> &nums)
+    {
+        std::unordered_map<int64_t, uint64_t> counter;
+        uint64_t answer = 0;
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            uint64_t buf = 0; 
+            int64_t num = nums[i];
+            while (num > 0)
+            {
+                buf = buf * 10 + num % 10;
+                num /= 10;
+            }
+
+            num = nums[i] - buf;
+            answer = (answer + counter[num]) % 1000000007;
+            counter[num]++;
+        }
+
+        return answer;
+    }
+};
+// @lc code=end
