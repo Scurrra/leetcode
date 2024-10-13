13427811aeb24230aace7f77303266d86839c4f6
Scurrra <iscurrra@gmail.com>
2024-10-13 15:11:21 +0300

    905. Sort Array By Parity

---
+++ src/cpp/905.sort-array-by-parity.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+/*
+ * @lc app=leetcode id=905 lang=cpp
+ *
+ * [905] Sort Array By Parity
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    std::vector<int> sortArrayByParity(std::vector<int> nums)
+    {
+        int l = 0, r = nums.size() - 1;
+        while (l < r && l < nums.size() && r >= 0)
+        {
+            while (l < nums.size() && nums[l] % 2 == 0)
+            {
+                l++;
+            }
+            while (r >= 0 && nums[r] % 2 == 1)
+            {
+                r--;
+            }
+
+            if (l < r)
+            {
+                std::swap(nums[l], nums[r]);
+                l++;
+                r--;
+            }
+        }
+
+        return nums;
+    }
+};
+// @lc code=end
